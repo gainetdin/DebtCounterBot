be55b2963583f9e2b3846f60566ff7ee7643fbd3
gainetdin <gainetdin@ya.ru>
2021-12-16 23:05:51 +0300

    Added unstaged files from previous commit

--- src/main/java/com/gainetdin/telegram/bot/DebtAccounterBot.java
+++ src/main/java/com/gainetdin/telegram/bot/DebtAccounterBot.java
@@ -1,11 +1,13 @@
 package com.gainetdin.telegram.bot;
 
+import com.gainetdin.telegram.data.ChatData;
+import com.gainetdin.telegram.process.DebtProcessor;
+import com.gainetdin.telegram.process.MessageSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -31,13 +33,13 @@ public class DebtAccounterBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        Long chatId = update.getMessage().getChatId();
-        String messageToBot = update.getMessage().getText();
-        SendMessage sendSomething = new SendMessage();
-        sendSomething.setChatId(String.valueOf(chatId));
-        sendSomething.setText(messageToBot);
+        //need to assign object creation to Spring
+        ChatData chatData = new ChatData();
+        chatData.setChatId(update.getMessage().getChatId());
+        chatData.setMessageToBot(update.getMessage().getText());
+        DebtProcessor.processData(chatData);
         try {
-            execute(sendSomething);
+            execute(MessageSender.generateMessage(chatData));
         } catch (TelegramApiException e) {
             log.error(e.getMessage());
         }
